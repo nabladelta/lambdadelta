84fc6ca2b42f63b42f9376c6ef9d19ce40efcac7
moot <moot@example.com>
2023-02-27 18:38:12 +0100

    Spec api

--- src/index.ts
+++ src/index.ts
@@ -9,35 +9,32 @@ const port = process.env.PORT
 const topic = process.env.TOPIC!
 const client = new BulletinBoard(process.env.SECRET!, topic, process.env.MEMSTORE == 'true')
 
-client.newThread().then(threadId => {
-  client.newMessage(threadId, {com: "Message 1"})
-  client.newMessage(threadId, {com: "Message 2"})
-})
-
 app.use(express.json())
 
-app.get('/:topic/thread/:id', async (req: Request, res: Response) => {
+app.get('/api/:topic/thread/:id', async (req: Request, res: Response) => {
     const thread = await client.getThreadContent(req.params.id)
     res.send({posts: thread})
 })
 
-app.post('/:topic/thread/:id', async (req: Request, res: Response) => {
+app.post('/api/:topic/thread/:id', async (req: Request, res: Response) => {
     await client.newMessage(req.params.id, req.body)
     const thread = await client.getThreadContent(req.params.id)
     res.send(thread)
 })
 
-app.post('/:topic', async (req: Request, res: Response) => {
+app.post('/api/:topic', async (req: Request, res: Response) => {
   const threadId = await client.newThread()
   await client.newMessage(threadId, req.body)
   const thread = await client.getThreadContent(threadId)
   res.send({op: threadId, thread: thread})
 })
 
-app.get('/:topic', async (req: Request, res: Response) => {
+app.get('/api/:topic', async (req: Request, res: Response) => {
   const threads = client.getThreadList()
   res.send({threads})
 })
+
+app.use('/', express.static('client/build'))
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
