692e3d55d1a8137436bfe13456f0edb73fcc03eb
moot <moot@example.com>
2023-02-27 14:00:40 +0100

    Add JSON middleware

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,23 @@ dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
-
-const client = new BulletinBoard(process.env.SECRET!, process.env.TOPIC!, process.env.MEMSTORE == 'true')
+const topic = process.env.TOPIC!
+const client = new BulletinBoard(process.env.SECRET!, topic, process.env.MEMSTORE == 'true')
 
 client.newThread().then(threadId => client.newMessage(threadId, "I am NUMBER ONE"))
 
+app.use(express.json())
 
-app.get('/thread/:id', async (req: Request, res: Response) => {
+app.get('/:topic/thread/:id', async (req: Request, res: Response) => {
     const thread = await client.getThreadCausalStream(req.params.id)
     res.send(thread.toString())
 })
 
+app.put('/:topic/thread/:id', async (req: Request, res: Response) => {
+    await client.newMessage(req.params.id, req.body)
+    const thread = await client.getThreadCausalStream(req.params.id)
+    res.send(thread.toString())
+})
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
 })
