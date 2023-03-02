4a51438b005cddbf37ae2043761bc99a474753f7
reilgun <87247753+reilgun@users.noreply.github.com>
2023-03-02 17:38:58 +0100

    Remake with Chakra

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import { ColorModeScript } from "@chakra-ui/react"
+import * as React from "react"
+import * as ReactDOM from "react-dom/client"
 import Home from './pages/home/home'
-import reportWebVitals from './reportWebVitals'
+import { App } from "./App"
+import reportWebVitals from "./reportWebVitals"
+import * as serviceWorker from "./serviceWorker"
+
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom"
-import { ChakraProvider } from '@chakra-ui/react'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,11 +27,17 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <RouterProvider router={router} />
-    </ChakraProvider>
-  </React.StrictMode>
+    <ColorModeScript />
+    <App>
+    <RouterProvider router={router} />
+    </App>
+  </React.StrictMode>,
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorker.unregister()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
