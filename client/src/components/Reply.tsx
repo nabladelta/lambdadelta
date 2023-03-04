b82e5050dec24e52fd484b06bde839fe704ccc01
reilgun <87247753+reilgun@users.noreply.github.com>
2023-03-04 14:23:47 +0100

    Fix input form

--- client/src/components/Reply.tsx
+++ client/src/components/Reply.tsx
@@ -22,8 +22,37 @@ import {
     DrawerCloseButton,
   } from '@chakra-ui/react'
 
+  type ModelElement = HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+
+function useModel<E extends ModelElement>(
+    initial?: string,
+    onChange?: React.ChangeEventHandler<E>
+    ) {
+    const [value, setValue] = React.useState<string>(initial || "")
+    const handler: React.ChangeEventHandler<E> = (e) => {
+        // Store the current value and run the callback function if provided
+        setValue(e.currentTarget.value)
+        onChange && onChange(e)
+    }
+    const model = { value, onChange: handler }
+    return { model, setModel: setValue }
+}
+
 function Reply({isOpen, onClose, onPost, op}: {op?: boolean, isOpen: boolean, onClose: () => void, onPost: (post: IPost) => void}) {
-    const [post, setPost] = useState<IPost>({com : "", sub: undefined, name: undefined, time: undefined, no: ""})
+
+    const name = useModel()
+    const sub = useModel()
+    const com = useModel()
+
+    function submit() {
+        onPost({
+            no: "",
+            time: Math.floor(Date.now()/1000),
+            com: com.model.value || "",
+            sub: sub.model.value || undefined,
+            name: name.model.value || undefined,
+        })
+    }
 
     const firstField: any = React.useRef()
     return (
@@ -37,40 +66,28 @@ function Reply({isOpen, onClose, onPost, op}: {op?: boolean, isOpen: boolean, on
             <DrawerOverlay />
             <DrawerContent>
                 <DrawerCloseButton />
-                <DrawerHeader borderBottomWidth='1px'>
-                    Write a new post
-                </DrawerHeader>
+                <DrawerHeader borderBottomWidth='1px'>New post</DrawerHeader>
                 <DrawerBody>
                     <Stack spacing='24px'>
                     <Box>
                         <FormLabel htmlFor='name'>Name</FormLabel>
-                        <Input
-                            value={post.name}
-                            id='name'
-                            placeholder='Anonymous'
-                            onChange={(e) => setPost((p) => {p.name = e.target.value; return p})}
-                        />
+                        <Input id='name' placeholder='Anonymous' {...name.model} />
                     </Box>
 
                     {op && <Box>
                         <FormLabel htmlFor='sub'>Subject</FormLabel>
-                        <Input
-                            value={post.sub}
-                            onChange={(e) => setPost((p) => {p.sub = e.target.value; return p})}
-                            id='sub' />
+                        <Input id='sub' {...sub.model} />
                     </Box>}
 
                     <Box>
                         <FormLabel htmlFor='desc'>Comment</FormLabel>
-                        <Textarea 
-                        onChange={(e) => setPost((p) => {p.com = e.target.value; return p})}
-                        size={'lg'} id='desc' ref={firstField} />
+                        <Textarea ref={firstField} size={'lg'} id='desc' {...com.model} />
                     </Box>
                     </Stack>
                 </DrawerBody>
                 <DrawerFooter borderTopWidth='1px'>
                     <Button variant='outline' mr={3} onClick={onClose}>Close</Button>
-                    <Button colorScheme={'gray'} onClick={() => {post.time = Math.floor(Date.now()/1000); onPost(post)}}>Post</Button>
+                    <Button colorScheme={'gray'} onClick={submit}>Post</Button>
                 </DrawerFooter>
             </DrawerContent>
         </Drawer>
