950851f57ae179a9336904cd6065076d11920e29
reilgun <87247753+reilgun@users.noreply.github.com>
2023-03-05 02:54:00 +0100

    Thread refactor

--- node/src/core/thread.ts
+++ node/src/core/thread.ts
@@ -3,7 +3,7 @@ import c from 'compact-encoding'
 import b4a from 'b4a'
 import Hypercore from 'hypercore'
 
-export function difference (setA: Set<any>, setB: Set<any>) {
+export function difference (setA: Set<any> | string[], setB: Set<any>) {
   const _difference = new Set(setA)
   for (const elem of setB) {
     _difference.delete(elem)
@@ -13,46 +13,35 @@ export function difference (setA: Set<any>, setB: Set<any>) {
 export class Thread {
   uid: string
   base: any
-  allow: any
   get: any
   storage: any
-  _inputKeys: any
-  _outputKeys: any
-  _streams: any
-  _ready: any
+  _inputKeys: Set<string>
+  _streams: Set<{stream: any, inputAnnouncer: any}>
+  _ready: Promise<void | void[]>
 
-  constructor (uid: string, base: any, allow: any, get: any, storage: any) {
+  constructor (uid: string, base: any, get: any, storage: any) {
     this.uid = uid
     this.base = base
-    this.allow = allow
     this.get = get
     this.storage = Hypercore.defaultStorage(storage)
 
     this._inputKeys = new Set()
-    this._outputKeys = new Set()
-    this._streams = []
+    this._streams = new Set()
 
     // Load storage
     this._ready = Promise.resolve().then(() => {
-      const coresToLoad = []
+        const coresToLoad = []
 
-      // Load local cores first
-      if (this.base.localInput) {
-        coresToLoad.push(this._addKeys([this.base.localInput.key.toString('hex')], 'input'))
-      }
-      if (this.base.localOutput) {
-        coresToLoad.push(this._addKeys([this.base.localOutput.key.toString('hex')], 'output'))
-      }
+        // Load local cores first
+        if (this.base.localInput) {
+          coresToLoad.push(this._addKeys([this.base.localInput.key.toString('hex')]))
+        }
 
-      // Load storage cores
-      coresToLoad.push(this.readStorageKeys())
+        // Load storage cores
+        coresToLoad.push(this.readStorageKeys())
 
-      return Promise.all(coresToLoad)
+        return Promise.all(coresToLoad)
     })
-      .then(() => Promise.all([
-        this._addKeys(this._inputKeys, 'input'),
-        this._addKeys(this._outputKeys, 'output')
-      ]))
   }
 
   ready () {
@@ -73,71 +62,52 @@ export class Thread {
 
     const inputAnnouncer = channel.addMessage({
       encoding: c.array(c.string),
-      async onmessage (msgs: any, session: any) {
-        const allowedKeys = msgs.filter((msg: any) => self.allow(msg, 'input', session))
+      async onmessage (msgs: string[], session: any) {
+        const allowedKeys = msgs.filter((msg: string) => self.allow(msg, session))
+        console.log("session", session)
         if (allowedKeys.length) {
           // Check if any are new
           const newKeys = difference(allowedKeys, self._inputKeys)
           if (newKeys.size > 0) {
-            await self._addKeys(newKeys, 'input')
+            await self._addKeys(newKeys)
             await self.updateStorageKeys()
           }
         }
       }
     })
 
-    const outputAnnouncer = channel.addMessage({
-      encoding: c.array(c.string),
-      async onmessage (msgs: any, session: any) {
-        const allowedKeys = msgs.filter((msg: any) => self.allow(msg, 'output', session))
-        if (allowedKeys.length) {
-          // Check if any are new
-          const newKeys = difference(allowedKeys, self._outputKeys)
-          if (newKeys.size > 0) {
-            await self._addKeys(newKeys, 'output')
-            await self.updateStorageKeys()
-          }
-        }
-      }
-    })
-
-    const streamRecord = { stream, inputAnnouncer, outputAnnouncer }
-    this._streams.push(streamRecord)
+    const streamRecord = { stream, inputAnnouncer }
+    this._streams.add(streamRecord)
     stream.once('close', () => {
-      this._streams.splice(this._streams.indexOf(streamRecord), 1)
+      this._streams.delete(streamRecord)
     })
 
-    if (this.base.localInput || this.base.inputs || this.base.outputs || this.base.localOutput) this.announce(streamRecord)
+    if (this.base.localInput || this.base.inputs) this.announce(streamRecord)
   }
 
-  async announce (stream: any) {
+  async allow(msg: string, session: any) {
+    return true
+  }
+
+  async announce(stream: { stream: any, inputAnnouncer: any}) {
     await this.ready()
 
     const keys = this.base.inputs.map((core: any) => core.key.toString('hex'))
     if (keys.length) {
-      // console.log('[' + this.base.localOutput.key.toString('hex').slice(-6) +
-      //       '] announce keys', keys.map((key) => key.slice(-6)))
       stream.inputAnnouncer.send(keys)
-    }
-
-    const outputKeys = this.base.outputs.map((core: any) => core.key.toString('hex'))
-    if (outputKeys.length) {
-      // console.log('[' + this.base.localOutput.key.toString('hex').slice(-6) +
-      //       '] announce outputKeys', outputKeys.map((key) => key.slice(-6)))
-      stream.outputAnnouncer.send(outputKeys)
     }
   }
 
-  async announceAll () {
+  async announceAll() {
     for (const stream of this._streams) {
       await this.announce(stream)
     }
   }
 
-  async _addKeys (keys: any, destination: any) {
+  async _addKeys(keys: string[] | Set<string>) {
     // Get & Ready Cores
     const cores = await Promise.all(Array.from(keys).map(async (key) => {
-      const core = this.get(b4a.from(key as any, 'hex'))
+      const core = this.get(b4a.from(key, 'hex'))
       // Necessary for autobase id (aka the core's id) setup
       await core.ready()
       return core
@@ -145,43 +115,29 @@ export class Thread {
 
     // Add to the corresponding place in autobase
     for (const core of cores) {
-      if (destination === 'output') {
-        this._outputKeys.add(core.key.toString('hex'))
-
-        // Skip local output lest we get a 'Batch is out-of-date' error
-        if (this.base.localOutput.key === core.key) {
-          console.log('found local output, continuing')
-          continue
-        }
-
-        // Update output to ensure up to date before adding
-        // Get a 'Batch is out-of-date.' error otherwise
-        if (this.base.started) await this.base.view.update()
-
-        await this.base.addOutput(core)
-      } else {
-        if (core.fork != 0) {
-          console.log("Forked Core")
-        }
-        this._inputKeys.add(core.key.toString('hex'))
-        await this.base.addInput(core)
+      if (core.fork != 0) {
+        console.log("Forked Core")
       }
+      this._inputKeys.add(core.key.toString('hex'))
+      await this.base.addInput(core)
     }
   }
 
-  _getStorage (file: any) {
-    const MANAGER_DIR = 'thread-rep/' + this.uid + "/"
-    return this.storage(MANAGER_DIR + file)
+  async readStorageKeys() {
+    await this._readStorageKey('inputs', this._inputKeys)
+    await this._addKeys(this._inputKeys)
   }
 
-  readStorageKeys () {
-    return Promise.all([
-      this._readStorageKey('inputs', this._inputKeys),
-      this._readStorageKey('outputs', this._outputKeys)
-    ])
+  async updateStorageKeys() {
+    await this._updateStorageKey('inputs', this._inputKeys)
+    await this.announceAll()
   }
 
-  _readStorageKey (file: any, output: any) {
+  _getStorage (file: string) {
+    return this.storage('thread-rep/' + this.uid + '/' + file)
+  }
+
+  _readStorageKey (file: string, output: Set<string>) {
     const store = this._getStorage(file)
     return new Promise<void>((resolve, reject) => {
       store.stat(async (err: any, stat: any) => {
@@ -193,7 +149,7 @@ export class Thread {
         const len = stat.size
         for (let start = 0; start < len; start += 32) {
           await new Promise<void>((resolve, reject) => {
-            store.read(start, 32, function (err: any, buf: any) {
+            store.read(start, 32, function (err: any, buf: Buffer) {
               if (err) throw err
 
               output.add(buf.toString('hex'))
@@ -209,13 +165,7 @@ export class Thread {
     )
   }
 
-  async updateStorageKeys () {
-    await this._updateStorageKey('inputs', this._inputKeys)
-    await this._updateStorageKey('outputs', this._outputKeys)
-    await this.announceAll()
-  }
-
-  async _updateStorageKey (file: any, input: any) {
+  async _updateStorageKey (file: string, input: Set<string>) {
     const store = this._getStorage(file)
     let i = 0
     for (const data of input) {
