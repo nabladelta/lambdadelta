d04caf1feb0b653e3a8a46ff37f41c55f807da03
moot <moot@example.com>
2023-03-08 18:36:43 +0100

    Encode mime

--- node/src/core/filestore.ts
+++ node/src/core/filestore.ts
@@ -1,5 +1,6 @@
 import Hyperblobs from 'hyperblobs'
 import b4a from 'b4a'
+import c from 'compact-encoding'
 
 export class Filestore {
     private corestore: any
@@ -8,17 +9,28 @@ export class Filestore {
         this.corestore = corestore.namespace('filestore')
     }
 
-    public async store(tid: string, data: Buffer) {
-        const core = this.corestore.namespace('op').get({ name: tid})
+    public async store(tid: string, data: Buffer, mime?: string) {
+        const core = this.corestore.namespace('op').get({ name: tid })
         await core.ready()
         const blobs = new Hyperblobs(core)
-        return { cid: core.key.toString('hex'), blobId: await blobs.put(data) as BlobID }
+
+        const state = c.state()
+        c.string.preencode(state, mime)
+        c.buffer.preencode(state, data)
+        c.string.encode(state, mime)
+        c.buffer.encode(state, data)
+        
+        return { cid: core.key.toString('hex'), blobId: await blobs.put(state.buffer) as BlobID }
     }
 
-    public async retrieve(cid: string, id: BlobID): Promise<Buffer> {
+    public async retrieve(cid: string, id: BlobID): Promise<{mime: string, data: Buffer}> {
         const core = this.corestore.get(b4a.from(cid, 'hex'))
         await core.ready()
         const blobs = new Hyperblobs(core)
-        return await blobs.get(id)
+        const blob: Buffer = await blobs.get(id)
+        const state = { start: 0, end: blob.length, blob, cache: null }
+        const mime = c.string.decode(state)
+        const data = c.buffer.decode(state)
+        return {mime, data}
     }
 }
