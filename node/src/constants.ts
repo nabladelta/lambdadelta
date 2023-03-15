1db8dbdecd56a7533ccdff099bd4856b8ee1d122
reilgun <87247753+reilgun@users.noreply.github.com>
2023-03-15 21:35:07 +0100

    Change default thumb size to 400

--- node/src/constants.ts
+++ node/src/constants.ts
@@ -7,7 +7,7 @@ export const PORT = process.env.PORT || "1832"
 export const TOPICS = process.env.TOPICS || "a"
 export const SECRET = process.env.SECRET
 export const MEMSTORE = process.env.MEMSTORE || "false"
-export const THUMB_SIZE = parseInt(process.env.THUMB_SIZE || "512")
+export const THUMB_SIZE = parseInt(process.env.THUMB_SIZE || "400")
 
 export const THUMB_FORMAT = sharp.format[process.env.THUMB_FORMAT as keyof FormatEnum]?.output.file ? process.env.THUMB_FORMAT as keyof FormatEnum : "jpeg"
 
