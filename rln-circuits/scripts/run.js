6a23e87c893df8f019f4110ee86372fce57a204a
reilgun <87247753+reilgun@users.noreply.github.com>
2023-03-18 20:59:14 +0100

    Add print

--- rln-circuits/scripts/run.js
+++ rln-circuits/scripts/run.js
@@ -2,6 +2,10 @@ const { execSync } = require("child_process");
 const { mkdirSync, renameSync, rmSync } = require('fs');
 const path = require('path');
 
+function execPrint(cmd) {
+    console.log(execSync(cmd).toString())
+}
+
 function run(name, scheme, tauname) {
     const root = process.cwd()
     const tauFile = path.join(root, 'tau', tauname)
@@ -15,31 +19,36 @@ function run(name, scheme, tauname) {
     mkdirSync(path.join(dest, scheme), {recursive: true})
 
     process.chdir(build)
-    execSync(`circom ${circuitFile} --r1cs --wasm --sym`)
+    execPrint(`circom ${circuitFile} --r1cs --wasm --sym`)
 
     rmSync(path.join(dest, 'js'), {recursive: true, force: true})
     renameSync(path.join(build, `${name}_js`, `${name}.wasm`), path.join(build, `${name}_js`, `circuit.wasm`))
     renameSync(path.join(build, `${name}_js`), path.join(dest, 'js'))
 
-    execSync(`snarkjs r1cs export json ${name}.r1cs ${name}.r1cs.json`)
+    execPrint(`snarkjs r1cs export json ${name}.r1cs ${name}.r1cs.json`)
     if (scheme == 'groth16') {
-        execSync(`snarkjs groth16 setup ${name}.r1cs ${tauFile} ${path.join(setup, 'rln_0000.zkey')} `)
-        execSync(`snarkjs zkey contribute ${path.join(setup, 'rln_0000.zkey')} ${path.join(setup, 'rln_0001.zkey')} --name="First contribution" -v -e="Random entropy"`)
-        execSync(`snarkjs zkey contribute ${path.join(setup, 'rln_0001.zkey')} ${path.join(setup, 'rln_0002.zkey')} --name="Second contribution" -v -e="Random entropy 2"`)
-        execSync(`snarkjs zkey beacon ${path.join(setup, 'rln_0002.zkey')} ${path.join(setup, 'final.zkey')} 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon phase2"`)
+        execPrint(`snarkjs groth16 setup ${name}.r1cs ${tauFile} ${path.join(setup, 'rln_0000.zkey')} `)
+        execPrint(`snarkjs zkey contribute ${path.join(setup, 'rln_0000.zkey')} ${path.join(setup, 'rln_0001.zkey')} --name="First contribution" -v -e="Random entropy"`)
+        execPrint(`snarkjs zkey contribute ${path.join(setup, 'rln_0001.zkey')} ${path.join(setup, 'rln_0002.zkey')} --name="Second contribution" -v -e="Random entropy 2"`)
+        execPrint(`snarkjs zkey beacon ${path.join(setup, 'rln_0002.zkey')} ${path.join(setup, 'final.zkey')} 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon phase2"`)
     } else if (scheme == 'plonk') {
-        execSync(`snarkjs plonk setup ${name}.r1cs ${tauFile} ${path.join(setup, 'final.zkey')} `)
+        execPrint(`snarkjs plonk setup ${name}.r1cs ${tauFile} ${path.join(setup, 'final.zkey')} `)
     } else if (scheme == 'fflonk') {
-        execSync(`snarkjs fflonk setup ${name}.r1cs ${tauFile} ${path.join(setup, 'final.zkey')} `)
+        execPrint(`snarkjs fflonk setup ${name}.r1cs ${tauFile} ${path.join(setup, 'final.zkey')} `)
     } else {
         console.error("No scheme")
         return
     }
-    execSync(`snarkjs zkey export verificationkey ${path.join(setup, 'final.zkey')} ${path.join(dest, scheme, 'verification_key.json')}`)
-    execSync(`snarkjs zkey export solidityverifier ${path.join(setup, 'final.zkey')}  ${path.join(dest, scheme, 'verifier.sol')}`)
+    execPrint(`snarkjs zkey export verificationkey ${path.join(setup, 'final.zkey')} ${path.join(dest, scheme, 'verification_key.json')}`)
+    execPrint(`snarkjs zkey export solidityverifier ${path.join(setup, 'final.zkey')}  ${path.join(dest, scheme, 'verifier.sol')}`)
     renameSync(path.join(setup, 'final.zkey'), path.join(dest, scheme, 'final.zkey'))
     process.chdir(root)
     rmSync(build, {recursive: true})
 }
 
 run('rln', 'groth16', "powersOfTau28_hez_final_17.ptau")
+run('rln', 'plonk', "powersOfTau28_hez_final_17.ptau")
+run('rln-multi', 'plonk', "powersOfTau28_hez_final_17.ptau")
+run('rln-multi', 'groth16', "powersOfTau28_hez_final_17.ptau")
+run('rln-same-dual', 'groth16', "powersOfTau28_hez_final_17.ptau")
+run('rln-same-dual', 'plonk', "powersOfTau28_hez_final_17.ptau")
