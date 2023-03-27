4db94b906fddb15d43025eb8bb07928a00e49625
reilgun <87247753+reilgun@users.noreply.github.com>
2023-03-27 02:12:25 +0200

    Implement verify and prove

--- node/src/core/rln/lambda.ts
+++ node/src/core/rln/lambda.ts
@@ -1,26 +1,45 @@
+import { BigNumberish } from "@semaphore-protocol/group"
 import { Identity } from "@semaphore-protocol/identity"
 import { SECRET } from "../../constants"
 import { GroupDataProvider } from "./providers/dataProvider"
 import { FileProvider } from "./providers/file"
-import { generateProof } from "./benchmarks/bindings/rln"
-import { generateDualProof } from "./rln_same_dual"
+import { generateProof, nullifierInput, RLNGFullProof, verifyProof } from "./rln"
 import { getZKFiles } from "./utils/files"
+
+export enum VerificationResult {
+    VALID,
+    INVALID,
+    MISSING_ROOT,
+    OUT_OF_RANGE
+}
 
 /**
  * Verifies RLN proofs
  */
 class Lambda {
     private provider: GroupDataProvider
-    
+    private settings: {vKey: any, scheme: "groth16" | "plonk"}
+    public expiredTolerance: number
+
     private constructor(provider: GroupDataProvider) {
+        this.settings = getZKFiles('rln-multiplier-generic', 'groth16')
         this.provider = provider
+        this.expiredTolerance = 0
     }
     public static async load() {
         const provider = await FileProvider.load()
         return [new Lambda(provider), new Delta(provider)]
     }
-    public async verifyProof() {
-        
+    public async verifyProof(proof: RLNGFullProof, claimedTime?: number) {
+        const root = proof.snarkProof.publicSignals.merkleRoot
+        const [start, end] = await this.provider.getRootTimeRange(root.toString())
+        if (!start) return VerificationResult.MISSING_ROOT
+        const result = await verifyProof(proof, this.settings)
+        if (!result) return VerificationResult.INVALID
+        if (!claimedTime) return VerificationResult.VALID
+        if (!end && claimedTime >= start) return VerificationResult.VALID
+        if (end && claimedTime >= start && claimedTime <= (end + this.expiredTolerance)) return VerificationResult.VALID
+        return VerificationResult.OUT_OF_RANGE
     }
 }
 /**
@@ -29,16 +48,30 @@ class Lambda {
 class Delta {
     private provider: GroupDataProvider
     private identity: Identity
-    private files: {wasmFilePath: string, zkeyFilePath: string}
+    private settings: {
+        userMessageLimitMultiplier: number,
+        scheme: 'groth16' | 'plonk'
+        wasmFilePath: string
+        zkeyFilePath: string
+    }
 
     public constructor(provider: GroupDataProvider) {
         this.provider = provider
         this.identity = new Identity(SECRET)
-        const {files} = getZKFiles('rln', 'groth16')
-        this.files = files
+        const {files, scheme} = getZKFiles('rln-multiplier-generic', 'groth16')
+        this.settings = {...files, userMessageLimitMultiplier: this.provider.getMultiplier(this.identity.commitment)!, scheme}
     }
-    public async createProof(signal: string, externalNullifier: string, rlnIdentifier: string, ) {
-        const merkleProof = this.provider.createMerkleProof(this.identity.commitment)
-        return await generateProof(this.identity, merkleProof, externalNullifier, signal, this.files, rlnIdentifier, 'groth16')
+    public async createProof(signal: string, externalNullifiers: nullifierInput[], rlnIdentifier: string) {
+        const merkleProof = this.provider.createMerkleProof(this.identity.commitment, this.settings.userMessageLimitMultiplier)
+
+        return await generateProof(
+            this.identity,
+            merkleProof,
+            externalNullifiers,
+            signal,
+            {
+                rlnIdentifier,
+               ...this.settings
+            })
     }
 }
