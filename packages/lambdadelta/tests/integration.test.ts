b4bec5cb83d3186c7ab8497273e36b203fe248e3
reilgun <87247753+reilgun@users.noreply.github.com>
2023-04-10 17:03:10 +0200

    Add messaging

--- packages/lambdadelta/tests/integration.test.ts
+++ packages/lambdadelta/tests/integration.test.ts
@@ -1,6 +1,7 @@
 import 'jest'
 import { LDNode } from '../src/node'
 import { findMissingPeers, findMissingPeersInFeed, findMissingTopics, nodeSetup, sleep } from './utils'
+import { VerificationResult } from 'bernkastel-rln'
 
 const TOPICS = ['a', 'b', 'c', 'd']
 
@@ -30,15 +31,35 @@ describe('LDNode', () => {
 
     jest.setTimeout(120000)
 
-    it('Join a topic', async () => {
+    it('Join a topic and post', async () => {
         for (const node of nodes) {
             await node.join([T])
         }
-
+        
         await sleep(10000)
 
         expect(findMissingPeers(nodes).length).toBe(0)
         expect(findMissingTopics(nodes, [T]).length).toBe(0)
         expect(findMissingPeersInFeed(nodes, [T]).length).toBe(0)
+
+        const feeds = nodes.map(n => n.getTopic(T)!)
+        const messages = [0, 1, 2].map(n => Buffer.from(`test ${n}`))
+        let i = 0
+        for (const feed of feeds) {
+            feed.on('syncEventReceivedTime', async (peerId, eventID, result) => {
+                console.log(`[FEED]: ${peerId} ${eventID} ${result}`)
+            })
+            expect(feed.getPeerList().length).toBe(2)
+
+            expect(await feed.newEvent("POST", messages[i++]))
+            .toEqual(VerificationResult.VALID)
+        }
+        await sleep(10000)
+
+        for (const feed of feeds) {
+            console.log(
+                (await feed.getEvents())
+                .map(e => e.content.toString()))
+        }
     })
 })
