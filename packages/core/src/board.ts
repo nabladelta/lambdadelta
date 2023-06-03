581484489da37d7e75b0c75daace8f846685f51b
reilgun <87247753+reilgun@users.noreply.github.com>
2023-06-03 19:10:16 +0200

    Start core work

---
+++ packages/core/src/board.ts
@@ -0,0 +1,7 @@
+import { Lambdadelta }  from "@bernkastel/lambdadelta"
+
+export class BulletinBoard extends Lambdadelta {
+    protected async validateContent(eventID: string, eventType: string, buf: Buffer): Promise<boolean> {
+        return false
+    }
+}
--- packages/core/src/node.ts
+++ packages/core/src/node.ts
@@ -1,6 +1,12 @@
-import { LDNode }  from "@bernkastel/lambdadelta"
+import { LDNode, Lambdadelta }  from "@bernkastel/lambdadelta"
+import { BulletinBoard } from "./board"
 
 class BBNode extends LDNode {
     public static appID = "BBS"
     public static protocolVersion = "1"
+
+    protected newFeed(topicHash: string): Lambdadelta {
+        return new BulletinBoard(topicHash, this.corestore, this.rln!)
+    }
 }
+
