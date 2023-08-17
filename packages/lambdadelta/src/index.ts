3b5ea8d8158c2baf5d93bb3b7820dfd44dee30e7
reilgun <87247753+reilgun@users.noreply.github.com>
2023-08-17 20:17:17 +0200

    Export all new classes

--- packages/lambdadelta/src/index.ts
+++ packages/lambdadelta/src/index.ts
@@ -2,4 +2,6 @@ import { Lambdadelta, NullifierSpec, LogEntry, PeerData } from "./lambdadelta"
 import { generateMemberCID, verifyMemberCIDProof } from "./membercid"
 import { LDNode, LDNodeBase } from "./node"
 import { Timeline } from "./timeline"
-export { Lambdadelta, generateMemberCID, verifyMemberCIDProof, LDNode, LDNodeBase, NullifierSpec, Timeline, LogEntry, PeerData }
+import { RelayerNodeBase, LDRelayerNode } from "./dandelion/relayerNode"
+import { RelayedLambdadelta } from "./dandelion/relayedFeed"
+export { Lambdadelta, generateMemberCID, verifyMemberCIDProof, LDNode, LDNodeBase, NullifierSpec, Timeline, LogEntry, PeerData, RelayerNodeBase, LDRelayerNode, RelayedLambdadelta }
